a09089d02cc941ac73fdd277787369fc23f58fe3
kutmicro2017 <32184745+kutmicro2017@users.noreply.github.com>
2017-10-12 18:47:20 +0900

    Update sketch_sep27a.ino

--- sketch_sep27a.ino
+++ sketch_sep27a.ino
@@ -1,18 +1,19 @@
-#define Right_G 1       //우측 신호등
+#define Right_G 1   //우측 신호등
 #define Right_Y 2
 #define Right_R 3
-#define Up_G 4          //위측 신호등
+#define Up_G 4      //위측 신호등
 #define Up_Y 5
 #define Up_R 6
-#define Left_G 7       //좌측 신호등
+#define Left_G 7    //좌측 신호등
 #define Left_Y 8
 #define Left_R 9
-#define Down_G 10      //아래쪽 신호등
+#define Down_G 10   //아래쪽 신호등
 #define Down_Y 11
 #define Down_R 12
-#define Botton_1 20    //스위치
+#define Botton_1 20 //스위치
 int interval = 30000;
-volatile bool state1 = true;  //스위치를 눌렀을 때 false 스위치를 안 눌렀을 때 true
+int BlinkYellow = 5;  //노란불 점멸 카운트
+volatile bool state1 = true;  //스위치를 누르면 false로 변환
 
 void setup() {
   pinMode(Right_G, OUTPUT);
@@ -27,64 +28,78 @@ void setup() {
   pinMode(Down_G, OUTPUT);
   pinMode(Down_Y, OUTPUT);
   pinMode(Down_R, OUTPUT);
-  pinMode(Botton_1, INPUT_PULLUP);        //input과의 차이는 프로세서 내부에 있는 pull-up 저항을 사용
-  attachInterrupt(3, Botton, FALLING);    //인터럽트 지정함수(어느핀으로 인터럽트 처리, 호출함수 이름, 모드(신호가 high에서 low))
+  pinMode(Botton_1, INPUT_PULLUP);  //input과의 차이는 프로세서 내부에 있는 pull-up 저항을 사용
+  attachInterrupt(3, Botton, FALLING);  //인터럽트 지정함수(어느핀으로 인터럽트 처리, 호출함수 이름, 모드(신호가 high에서 low))
 }
 
 void loop() {
-  digitalWrite(Right_G, HIGH);   //처음 신호
+  unsigned long CheckTime = millis(); //타이머 함수, deley를 이용하여도 millis()는 흐른다
+  digitalWrite(Right_G, HIGH);
   digitalWrite(Up_R, HIGH);
   digitalWrite(Left_R, HIGH);
   digitalWrite(Down_R, HIGH);
-  delay(30000);                  //대기
+  
+  delay(interval);
   digitalWrite(Right_G, LOW);
-  BlinkLED(Right_Y);             //점멸
+  BlinkLED(Right_Y);
   digitalWrite(Right_R, HIGH); 
   digitalWrite(Up_R, LOW);
   digitalWrite(Up_G, HIGH);
-  
-  unsigned long CheckTime = millis(); //타이머 함수 deley를 이용하여도 millis()는 흐른다
-  while(interval > (millis() - CheckTime)) {
-    if(state1 == false) { //위측 신호등이 녹색일 동안에 스위치를 누를 경우 진행
-      delay(3000);
-      digitalWrite(Up_G, LOW);
-      BlinkLED(Up_Y);
-      digitalWrite(Up_R, HIGH);
-      state1 = true;
-    }
-    delay(5000); // if문 안돌 때 5초 기다렷다가 다시 if문 확인
-  }
-  if(digitalRead(Up_G) == HIGH) { //스위치가 눌리지 않았을 경우 진행
-    digitalWrite(Up_G, LOW);
-    BlinkLED(Up_Y);
-    digitalWrite(Up_R, HIGH);
-  }
-  
+  //Up 스위치를 제거할 경우 사용
+  /*delay(interval);
+  digitalWrite(Up_G, LOW);
+  BlinkLED(Up_Y);
+  digitalWrite(Up_R, HIGH);*/
+  CheckTime = millis();
+  SwichLoop(Up_G, Up_Y, Up_R, CheckTime);
   digitalWrite(Left_R, LOW);
   digitalWrite(Left_G, HIGH);
-  delay(30000);
+  
+  delay(interval);
   digitalWrite(Left_G, LOW);
   BlinkLED(Left_Y);
   digitalWrite(Left_R, HIGH);
   digitalWrite(Down_R, LOW);
   digitalWrite(Down_G, HIGH);
-  delay(30000);
+  
+  delay(interval);
   digitalWrite(Down_G, LOW);
   BlinkLED(Down_Y);
   digitalWrite(Down_R, HIGH);
   digitalWrite(Right_R, LOW);
 }
 
-void Botton() { //인터럽트 발생시 호출(스위치를 누를 경우 호출)
+// 인터럽트 발생시 호출(스위치를 누를 경우 호출)
+void Botton() {
   if(digitalRead(Up_G) == HIGH)
   state1 = false;
 }
 
-void BlinkLED(const int YellowLED) {  //점멸
-   for(int i =0; i < 5; i++) {
+// 노란불 점멸 함수
+void BlinkLED(const int YellowLED) {
+   for(int i =0; i < BlinkYellow; i++) {
     digitalWrite(YellowLED, HIGH);
     delay(500);
     digitalWrite(YellowLED, LOW);
     delay(500);
    }
+}
+
+// 스위치 함수
+void SwichLoop(int GLight, int YLight, int RLight, long CheckTime) {
+  while(interval > (millis() - CheckTime)) {  //interval 시간 동안 진행
+    if(state1 == false) { //위측 신호등이 녹색등일 동안에 스위치를 누를 경우 진행
+      delay(3000);
+      digitalWrite(GLight, LOW);
+      BlinkLED(YLight);
+      digitalWrite(RLight, HIGH);
+      state1 = true;
+    }
+    delay(500);
+  }
+  if(digitalRead(GLight) == HIGH) { //스위치가 눌리지 않았을 경우 진행
+    digitalWrite(GLight, LOW);
+    BlinkLED(YLight);
+    digitalWrite(RLight, HIGH);
+  }
 }
