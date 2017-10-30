e6612f94d1812fd174bb2c8c6b22223341083536
kutmicro2017 <32184745+kutmicro2017@users.noreply.github.com>
2017-10-30 20:41:55 +0900

    Update 과제1_2번.ino

--- 과제1_2번.ino
+++ 과제1_2번.ino
@@ -1,5 +1,7 @@
-const int row[8] = {26, 27, 28, 29, 30, 31, 32, 33 };
-const int col[8] = {38, 39, 40, 41, 42, 43, 44, 45 };
+const int row[8] = {22, 23, 24, 25, 26, 27, 28, 29};
+const int col[8] = {30, 31, 32, 33, 34, 35, 36, 37};
+const int row2[8] = {38, 39, 40, 41, 42, 43, 44, 45};
+const int col2[8] = {46, 47, 48, 49, 19, 18, 17, 16};
 
 int number[10][8][4] = {
   {0, 1, 1, 0,  //0
@@ -102,22 +104,27 @@ int PTLight[2][8][8] = {
    0, 0, 1, 0, 0, 0, 1, 0}
 };
 
-#define Right_G 1  	 //우측 신호등
+#define Right_G 1   //우측 신호등
 #define Right_Y 2
 #define Right_R 3
-#define Up_G 4      	//위쪽 신호등
+#define Up_G 4      //위측 신호등
 #define Up_Y 5
 #define Up_R 6
-#define Left_G 7    	//좌측 신호등
+#define Left_G 7    //좌측 신호등
 #define Left_Y 8
 #define Left_R 9
-#define Down_G 10     //아래쪽 신호등
+#define Down_G 10   //아래쪽 신호등
 #define Down_Y 11
 #define Down_R 12
-#define Botton_1 20 	//스위치
-int interval = 30000;	//시간
-int BlinkYellow = 3;  	//노란불 점멸 카운트
+#define Botton_1 20 //스위치
+long interval;
+long IntervalSet = 20000;
+long IntervalAdd = 10000;
+long BlinkYellow = 3;  //노란불 점멸 카운트
+int SDelay = 3000;
 volatile bool state1 = true;  //스위치를 누르면 false로 변환
+
+long CheckTime;
 
 void setup() {
   pinMode(Right_G, OUTPUT);
@@ -132,61 +139,66 @@ void setup() {
   pinMode(Down_G, OUTPUT);
   pinMode(Down_Y, OUTPUT);
   pinMode(Down_R, OUTPUT);
-  pinMode(Botton_1, INPUT_PULLUP);  //input과의 차이는 프로세서 내부에 있는 pull-up 저항을 사용
-  attachInterrupt(3, Botton, FALLING);  //인터럽트 지정함수(어느핀으로 인터럽트 처리, 호출함수 이름, 모드(신호가 high에서 low))
+  pinMode(Botton_1, INPUT_PULLUP);       //input과의 차이는 프로세서 내부에 있는 pull-up 저항을 사용
+  attachInterrupt(3, Botton, FALLING);   //인터럽트 지정함수(어느핀으로 인터럽트 처리, 호출함수 이름, 모드(신호가 high에서 low))
   for (int thisPin = 0; thisPin < 8; thisPin++) //도트메트릭스에 연결한 것들
    {
     pinMode(row[thisPin], OUTPUT);
     pinMode(col[thisPin], OUTPUT);
+    pinMode(row2[thisPin], OUTPUT);
+    pinMode(col2[thisPin], OUTPUT);
    }
 }
 
 void loop() {
-  unsigned long CheckTime = millis(); //타이머 함수, deley를 이용하여도 millis()는 흐른다
+  //unsigned long CheckTime = millis(); //타이머 함수, deley를 이용하여도 millis()는 흐른다
+  int Time;
+  interval = IntervalSet;
   digitalWrite(Right_G, HIGH);
   digitalWrite(Up_R, HIGH);
   digitalWrite(Left_G, HIGH);
   digitalWrite(Down_R, HIGH);
  
-  CheckTime = millis();//초록불일 때 보행자 신호는 사람 모습 출력
-  while(interval > (millis() - CheckTime)) {
-    PSignal(0);
-  }
-  //delay(interval);
-  digitalWrite(Right_G, LOW);
-  digitalWrite(Left_G, LOW);
-
-  BlinkLED(Right_Y, Left_Y);
-  digitalWrite(Right_R, HIGH);
-  digitalWrite(Left_R, HIGH);
-  
+  CheckTime = millis();
+  SwitchLoop(Right_G, Right_Y, Right_R, Left_G, Left_Y, Left_R);
   digitalWrite(Up_R, LOW);
   digitalWrite(Down_R, LOW);
   digitalWrite(Up_G, HIGH);
   digitalWrite(Down_G, HIGH);
 
-  /*
- CheckTime = millis();
+  CheckTime = millis();
   while(interval > (millis() - CheckTime)) {
     PSignal(0);
+    Time = (interval - (millis() - CheckTime)) / 1000;
+    /*
+    if(state1 == false){
+        interval += IntervalAdd;
+        delay(100);
+    }
+    CleanDOT();
+    state1 = true;
+    */
+    NSignal(Time);
   }
+  interval = IntervalSet;
   digitalWrite(Up_G, LOW);
   digitalWrite(Down_G, LOW);
   BlinkLED(Up_Y, Down_Y);
   digitalWrite(Up_R, HIGH);
   digitalWrite(Down_R, HIGH);
-  */
-
-  CheckTime = millis();
-  SwitchLoop(Up_G, Up_Y, Up_R, Down_G, Down_Y, Down_R, CheckTime);
+  
   digitalWrite(Left_R, LOW);
   digitalWrite(Right_R, LOW);
   
 }
 
-// 인터럽트 발생 시 호출(스위치를 누를 경우 호출)
+// 인터럽트 발생시 호출(스위치를 누를 경우 호출)
 void Botton() {
-  if(digitalRead(Up_G) == HIGH)
+  /*
+  if(digitalRead(Right_G) == HIGH){
+  state1 = false;
+  }
+  */
   state1 = false;
 }
 
@@ -197,30 +209,36 @@ void BlinkLED(const int YellowLED1, const int YellowLED2) {
     digitalWrite(YellowLED1, HIGH);
     digitalWrite(YellowLED2, HIGH);
     Time = millis();
-    while((millis() - Time) <= 500){
+    while((millis() - Time) <= 500)
+    {
       PSignal(0);
+      //NSignal((BlinkYellow - i));
     }
     digitalWrite(YellowLED1, LOW);
     digitalWrite(YellowLED2, LOW);
     Time = millis();
-    while((millis() - Time) <= 500){
+    while((millis() - Time) <= 500)
+    {
       PSignal(0);
+      //NSignal((BlinkYellow - i));
     }
    }
 }
 
 // 스위치 함수
-void SwitchLoop(int GLight1, int YLight1, int RLight1, int GLight2, int YLight2, int RLight2, long CheckTime) {
+void SwitchLoop(int GLight1, int YLight1, int RLight1, int GLight2, int YLight2, int RLight2) {
   int Time;
-  long DTime;	//스위치 누르면 3초 대기
-  int FTime;	//시간 앞자리
-  int LTime;	//시간 뒷자리
+  long DTime;
+  
+  state1 = true;
   while(interval > (millis() - CheckTime)) {  //interval 시간 동안 진행
-    if(state1 == false) { //위 측 신호등이 녹색등일 동안에 스위치를 누를 경우 진행
-      
-      DTime = millis();//스위치 누르면 대기
-      while(3000 > (millis() - DTime)){ //3초 동안 돌린다는 의미
-       PSignal(0);//사람 모습 출력
+    if(state1 == false) { //위측 신호등이 녹색등일 동안에 스위치를 누를 경우 진행
+      DTime = millis();
+      while(SDelay > (millis() - DTime)){ 
+       PSignal(0);
+       Time = (SDelay - (millis() - DTime)) / 1000;
+       NSignal(Time);
+       CleanDOT();
       }
       digitalWrite(GLight1, LOW);
       digitalWrite(GLight2, LOW);
@@ -229,16 +247,27 @@ void SwitchLoop(int GLight1, int YLight1, int RLight1, int GLight2, int YLight2,
       digitalWrite(RLight2, HIGH);
       state1 = true;
       //CheckTime = millis();
-      while(interval > (millis() - CheckTime)){//도트메트릭스 출력하기
-      //PSignal(1);
+      while(interval > (millis() - CheckTime))
+      {
+        PSignal(1);
         Time = (interval - (millis() - CheckTime)) / 1000;
-        FTime = Time / 10;	//시간 앞자리 구하기
-        LTime = Time % 10;	//시간 뒷자리 구하기
-        NSignal(FTime, LTime);	//도트 메트릭스에 숫자 출력
+        /*
+        if(state1 == false){
+           interval += IntervalAdd;
+           delay(100);
+        }
+        */
+        NSignal(Time);
+        CleanDOT();
+        state1 = true;
       }
-      CleanDOT();//다 지우고
+      CleanDOT();
     }
-    PSignal(0);//사람 모습 출력
+    PSignal(0);
+    Time = (interval - (millis() - CheckTime)) / 1000;
+    NSignal(Time);
+    interval = IntervalSet;
+    CleanDOT();
   }
   if(digitalRead(GLight1) == HIGH) { //스위치가 눌리지 않았을 경우 진행
     digitalWrite(GLight1, LOW);
@@ -247,41 +276,57 @@ void SwitchLoop(int GLight1, int YLight1, int RLight1, int GLight2, int YLight2,
     digitalWrite(RLight1, HIGH);
     digitalWrite(RLight2, HIGH);
   }
+  else
+    BlinkLED(YLight1, YLight2);
 } 
 //도트 매트릭스 전체를 정리 함수
-void CleanDOT(){
-  for(int i = 0; i < 8; i++){
+void CleanDOT()
+{
+  for(int i = 0; i < 8; i++)
+  {
      digitalWrite(row[i], LOW);
      digitalWrite(col[i], HIGH);
+     digitalWrite(row2[i], LOW);
+     digitalWrite(col2[i], HIGH);
   }
 }
 // 남은 시간으로 도트 메트릭스에 숫자 출력하는 함수
-void NSignal(int FTime, int LTime){
-  for(int i = 0; i < 8; i++){
-    //CleanDOT(); //신호등 불빛이 너무 약하면 밑에 CleanDOT지우고 여기다가 쓴다.
-    for(int j = 0; j < 4; j++){
-      CleanDOT();
-      digitalWrite(row[i], HIGH);//가로줄 다 켜놓고
-      if(number[FTime][i][j]){//십의 자리 숫자
-       digitalWrite(col[j], LOW);//세로줄을 하나씩 켜준다.
-       delay(1);//0.001초 동안
+void NSignal(int Time)
+{
+  int FTime;
+  int LTime;
+
+  FTime = Time / 10;
+  LTime = Time % 10;
+  for(int i = 0; i < 8; i++)
+  {
+    CleanDOT();
+    for(int j = 0; j < 4; j++)
+    {
+      digitalWrite(row[i], HIGH);
+      if(number[FTime][i][j]){
+       digitalWrite(col[j], LOW);
+       delay(1);
       }
-      if(number[LTime][i][j]){//일의 자리 숫자씩
-        digitalWrite(col[j+4], LOW);//세로줄을 하나씩 켜준다.
-        delay(1);//0.001초 동안
+      if(number[LTime][i][j]){
+        digitalWrite(col[j+4], LOW);
+        delay(1);
       }
     }
   }
+  CleanDOT();
 }
 //사람 모습 출력 함수
-void PSignal(int GoStop){
-  for(int i = 0; i < 8; i++){
-    //CleanDOT(); //신호등 불빛이 너무 약하면 밑에 CleanDOT지우고 여기다가 쓴다.
-    for(int j = 0; j < 8; j++){
-      CleanDOT();//전체삭제
-      digitalWrite(row[i], HIGH);//가로줄 전체 다 켜놓고
+void PSignal(int GoStop)
+{
+  for(int i = 0; i < 8; i++)
+  {
+    CleanDOT();
+    for(int j = 0; j < 8; j++)
+    {
+      digitalWrite(row2[i], HIGH);
       if(PTLight[GoStop][i][j] == 1){
-       digitalWrite(col[j], LOW);
+       digitalWrite(col2[j], LOW);
         delay(1);
       }
     }
